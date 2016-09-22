500b80ee19ec9966e63cf4365764d1fee59a0cfa
phoet <phoetmail@googlemail.com>
2016-09-22 21:25:48 +0200

    move the logic into it's own file

--- lib/whitelabel.rb
+++ lib/whitelabel.rb
@@ -1,52 +1,10 @@
 require "whitelabel/version"
+require "whitelabel/handler"
 
 require "yaml"
 
 module Whitelabel
   class << self
-    attr_accessor :labels
-
-    def from_file(path)
-      @labels = File.open(path) { |file| YAML.load(file) }
-    end
-
-    def label
-      Thread.current[:whitelabel]
-    end
-
-    def label=(label)
-      Thread.current[:whitelabel] = label
-    end
-
-    def label_for(pattern, identifier=:label_id)
-      self.label = find_label(pattern, identifier)
-    end
-
-    def find_label(pattern, identifier=:label_id)
-      @labels.find { |label| label.send(identifier) =~ /^#{pattern}$/ }
-    end
-
-    def with_label(tmp=nil)
-      if tmp
-        current_label = self.label
-        self.label = tmp
-      end
-      yield
-    ensure
-      self.label = current_label if current_label
-    end
-
-    def each_label(&block)
-      labels.map { |label| with_label(label, &block) }
-    end
-
-    def [](accessor)
-      raise "set a label before calling '#{accessor}'" if self.label.nil?
-      self.label.send :"#{accessor}"
-    end
-
-    def reset!
-      Thread.current[:whitelabel] = nil
-    end
+    include Whitelabel::Handler
   end
 end
---
+++ lib/whitelabel/handler.rb
@@ -0,0 +1,48 @@
+module Whitelabel
+  module Handler
+    attr_accessor :labels
+
+    def from_file(path)
+      @labels = File.open(path) { |file| YAML.load(file) }
+    end
+
+    def label
+      Thread.current[:whitelabel]
+    end
+
+    def label=(label)
+      Thread.current[:whitelabel] = label
+    end
+
+    def label_for(pattern, identifier=:label_id)
+      self.label = find_label(pattern, identifier)
+    end
+
+    def find_label(pattern, identifier=:label_id)
+      @labels.find { |label| label.send(identifier) =~ /^#{pattern}$/ }
+    end
+
+    def with_label(tmp=nil)
+      if tmp
+        current_label = self.label
+        self.label = tmp
+      end
+      yield
+    ensure
+      self.label = current_label if current_label
+    end
+
+    def each_label(&block)
+      labels.map { |label| with_label(label, &block) }
+    end
+
+    def [](accessor)
+      raise "set a label before calling '#{accessor}'" if self.label.nil?
+      self.label.send :"#{accessor}"
+    end
+
+    def reset!
+      Thread.current[:whitelabel] = nil
+    end
+  end
+end
